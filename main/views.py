774652cddf3be77c820a71887eabcf350c283b20
Hussien-Mahmoud <hussienmahmuod@gmail.com>
2022-12-13 00:25:55 +0200

    making home image dynamically changeable

--- main/views.py
+++ main/views.py
@@ -16,4 +16,5 @@ class MainView(BaseView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['about_image'] = AboutImage.objects.all()[0].image
+        context['home_image'] = HomeImage.objects.all()[0].image
         return context
